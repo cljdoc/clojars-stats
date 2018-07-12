55452694b8b7eba420f0a1a10e26f0931908ea94
Martin Klepsch <martinklepsch@googlemail.com>
2018-07-12 18:58:02 +0200

    dont restrict to 2018

--- resources/queries.sql
+++ resources/queries.sql
@@ -23,5 +23,5 @@ create table stats (
 -- :doc Monthly downloads for specified artifact
 SELECT strftime('%Y-%m', date) as timespan, SUM(downloads) as downloads
 FROM stats
-WHERE group_id = :group_id AND artifact_id = :artifact_id AND date LIKE '2018%'
+WHERE group_id = :group_id AND artifact_id = :artifact_id
 GROUP BY timespan;
