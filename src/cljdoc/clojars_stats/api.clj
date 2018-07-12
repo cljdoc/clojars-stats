5415754a9e749b3cc5477fbd84d47f875c582220
Martin Klepsch <martinklepsch@googlemail.com>
2018-07-12 17:06:41 +0200

    minimal home page

--- src/cljdoc/clojars_stats/api.clj
+++ src/cljdoc/clojars_stats/api.clj
@@ -2,25 +2,38 @@
   (:require [cljdoc.clojars-stats.db :as db]
             [ring.middleware.params :as params]
             [ring.adapter.jetty :as jetty]
-            [jsonista.core :as j]))
+            [jsonista.core :as j]
+            [clojure.string :as string]))
 
 (def exposed-queries
   {"/artifact-monthly" {:query-fn db/artifact-monthly
                         :params [:group_id :artifact_id]}})
 
+(def index-page
+  (->> ["<pre>"
+        "Various endpoints are available:\n"
+        (for [[k {:keys [params]}] exposed-queries]
+          (str k " query-params: " (mapv name params) "\n"))
+        "\nContribute more at https://github.com/cljdoc/clojars-stats"
+        "</pre>"]
+       flatten
+       (string/join "\n")))
+
 (defn make-handler [db]
   (fn handler [{:keys [uri query-params]}]
-    (if-let [{:keys [query-fn params]} (get exposed-queries uri)]
-      (if (every? query-params (map name params))
-        {:status 200
-         :body (->> (for [p params] [p (get query-params (name p))])
-                    (into {})
-                    (query-fn db)
-                    (j/write-value-as-string))}
-        {:status 400
-         :body (format "Insufficient parameters:\nProvided: %s\nExpected: %s" query-params params)})
-      {:status 404
-       :body (format "Unknown query: %s" uri)})))
+    (if (= "/" uri)
+      {:status 200 :body index-page}
+      (if-let [{:keys [query-fn params]} (get exposed-queries uri)]
+        (if (every? query-params (map name params))
+          {:status 200
+           :body (->> (for [p params] [p (get query-params (name p))])
+                      (into {})
+                      (query-fn db)
+                      (j/write-value-as-string))}
+          {:status 400
+           :body (format "Insufficient parameters:\nProvided: %s\nExpected: %s" query-params params)})
+        {:status 404
+         :body (format "Unknown query: %s" uri)}))))
 
 (defn start! [{:keys [db port]}]
   (assert (and db port))
@@ -30,7 +43,7 @@
 
 (comment
 
-  (def srv (start! cljdoc.clojars-stats/db))
+  (def srv (start! {:db cljdoc.clojars-stats/db :port 3000}))
 
   (.stop srv)
 
